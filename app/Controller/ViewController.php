5e21bafd778e868969c345bde7bd45edbd8176e4
promisingccy <2534539009@qq.com>
2019-09-09 19:58:19 +0800

    添加视图

---
+++ app/Controller/ViewController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ccy
+ * Date: 2019/9/9
+ * Time: 19:50
+ */
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\View\RenderInterface;
+
+/**
+ * @AutoController
+ */
+class ViewController
+{
+    public function index(RenderInterface $render)
+    {
+        return $render->render('index', ['name' => 'Hyperf']);
+    }
+}
